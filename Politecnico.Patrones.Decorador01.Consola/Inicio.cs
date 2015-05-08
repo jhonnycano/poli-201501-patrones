07045190de2767e750661ab9b316f98cf0a599d8
Jhonny Cano <jhonnycano@hotmail.com>
2015-05-08 12:14:49 -0500

    Creacion de proyectos para ejercicio Decorador01

---
+++ Politecnico.Patrones.Decorador01.Consola/Inicio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Politecnico.Patrones.Decorador01.Consola
+{
+    class Inicio
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
