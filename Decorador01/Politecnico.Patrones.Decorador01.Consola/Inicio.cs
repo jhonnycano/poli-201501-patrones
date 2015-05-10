291d2ed0699c114485010099be91fb9e6407c31b
Jhonny Cano <jhonnycano@hotmail.com>
2015-05-10 12:59:21 -0500

    arreglando ejercicio de decorador para usar desde Consola

--- Decorador01/Politecnico.Patrones.Decorador01.Consola/Inicio.cs
+++ Decorador01/Politecnico.Patrones.Decorador01.Consola/Inicio.cs
@@ -1,9 +1,30 @@
-﻿namespace Politecnico.Patrones.Decorador01.Consola
+﻿using System;
+
+namespace Politecnico.Patrones.Decorador01.Consola
 {
     internal class Inicio
     {
-        private static void Main(string[] args)
-        {
+        private static void Main() {
+            Console.Write(@"Ingrese cadena: ");
+            var entrada = Console.ReadLine();
+
+            var decoradores = PrepararDecoradores();
+            string result = decoradores.Decorar(entrada);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        private static DecoradorBarraHorizontalDespues PrepararDecoradores() {
+            return new DecoradorBarraHorizontalDespues(
+                new DecoradorFuenteHtml4(
+                    new DecoradorAcentos(
+                        new DecoradorSubrayado(
+                            new DecoradorCursiva(
+                                new DecoradorNegrilla()
+                                )
+                            )
+                        ),
+                    new DecoradorFuenteHtml4.Parametros {Color = "#f0f0e0", Tam = "16px", Tipo = "Times New Roman"})
+                );
         }
     }
 }
