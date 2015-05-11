a553f9831202a2ff0bd0d6e69e8bd2bde166b0b4
Jhonny Cano <jhonnycano@hotmail.com>
2015-05-11 16:13:55 -0500

    nada

--- Politecnico.Patrones.Decorador01.Tests/DecoradorHtmlTest.cs
+++ Politecnico.Patrones.Decorador01.Tests/DecoradorHtmlTest.cs
@@ -6,16 +6,35 @@ namespace Politecnico.Patrones.Decorador01.Tests
     internal class DecoradorHtmlTest
     {
         [Test]
+        public void DecoradorAcentos()
+        {
+            var cadena = new DecoradorAcentos();
+
+            string result = cadena.Decorar("Jaime Peña García");
+            Assert.AreEqual("Jaime Pe&ntilde;a Garc&iacute;a", result);
+        }
+
+        [Test]
+        public void DecoradorFuenteHtml4()
+        {
+            var cadena = new DecoradorFuenteHtml4(new DecoradorFuenteHtml4.Parametros {Color = "#333333", Tam = "16px", Tipo = "Garamond"});
+
+            string result = cadena.Decorar("En verdes praderas me hace recostar");
+            Assert.AreEqual("<font color='#333333' face='Garamond' size='16px'>En verdes praderas me hace recostar</font>", result);
+        }
+
+        [Test]
         public void VariosDecoradores()
         {
-            var cadena = new DecoradorBarraHorizontalDespues(
-                new DecoradorCursiva(
-                    new DecoradorNegrilla()
-                    )
-                );
+            var cadena = new DecoradorAcentos(
+                new DecoradorBarraHorizontalDespues(
+                    new DecoradorCursiva(
+                        new DecoradorNegrilla()
+                        )
+                    ));
 
-            string result = cadena.Decorar("hola mundo");
-            Assert.AreEqual("<em><strong>hola mundo</strong></em><hr />", result);
+            string result = cadena.Decorar("hola Mariú");
+            Assert.AreEqual("<em><strong>hola Mari&uacute;</strong></em><hr />", result);
         }
     }
 }
