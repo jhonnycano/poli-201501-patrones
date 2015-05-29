f2b3b39b861b24755cb734ebee9602de40a42083
Jhonny Cano <jhonnycano@hotmail.com>
2015-05-29 13:06:17 -0500

    Se implementa generar reportes en PDF o a pantalla

--- ProyectoFinal/Politecnico.Patrones.ProyectoFinal.Web/Controllers/ReportesController.cs
+++ ProyectoFinal/Politecnico.Patrones.ProyectoFinal.Web/Controllers/ReportesController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using Politecnico.Patrones.ProyectoFinal.Contratos;
 using Politecnico.Patrones.ProyectoFinal.Contratos.VO;
+using Politecnico.Patrones.ProyectoFinal.Web.Models;
+using Rotativa;
 
 namespace Politecnico.Patrones.ProyectoFinal.Web.Controllers {
     public class ReportesController : Controller {
@@ -17,17 +20,23 @@ namespace Politecnico.Patrones.ProyectoFinal.Web.Controllers {
             if (TempData.ContainsKey("mensaje")) {
                 ViewBag.Mensaje = TempData["mensaje"];
             }
-            return View();
-        }
 
-        public ActionResult Reporte(TipoReporte tipo) {
+            var definicion = TraerDefinicionReportes();
+            return View(definicion);
+        }
+        public ActionResult Reporte(TipoReporte tipo, int dest = 0) {
             var vistaParametros = TraerVistaParametros(tipo);
-            ViewBag.Tipo = tipo;
-            ViewBag.ParametrosReporte = vistaParametros;
+            var definicion = TraerDefinicionReportes();
+            ViewData["Tipo"] = tipo;
+            ViewData["Destino"] = dest;
+            ViewData["ParametrosReporte"] = vistaParametros;
+            ViewData["Nombre"] = definicion.First(r => r.Tipo == tipo).Etiqueta;
             return View();
         }
         public ActionResult Traer(FormCollection form) {
             var tipo = form["tipo"];
+            var destino = int.Parse(form["destino"]);
+
             TipoReporte tipoReporte;
             if (!Enum.TryParse(tipo, out tipoReporte))
                 return RedirectToAction("Index");
@@ -52,10 +61,27 @@ namespace Politecnico.Patrones.ProyectoFinal.Web.Controllers {
             var vista = salida.Consulta.Vista;
             dynamic objeto = salida.Consulta.Objeto;
 
-            return View(vista, objeto);
+            if (destino == 0) {
+                return View(vista, objeto);
+            }
+
+            ViewData["destino"] = destino;
+            var fch = DateTime.Now.ToString("yyyyMMdd_HHmmss_fff");
+            string archivo = tipoReporte + "_" + fch + ".pdf";
+            return new ViewAsPdf(vista, objeto) {FileName = archivo};
         }
         private string TraerVistaParametros(TipoReporte tipo) {
-            return "_" + tipo;
+            return "_Params" + tipo;
+        }
+        private static List<ReporteDefinicion> TraerDefinicionReportes() {
+            var modelo = new List<ReporteDefinicion>
+                {
+                    new ReporteDefinicion {Etiqueta = "Sencillos por rango de tiempo", Tipo = TipoReporte.ListaCanciones},
+                    new ReporteDefinicion {Etiqueta = "Álbumes por rango de tiempo", Tipo = TipoReporte.ListaAlbumes},
+                    new ReporteDefinicion {Etiqueta = "Detalle de intérprete", Tipo = TipoReporte.InterpreteDetalle},
+                    new ReporteDefinicion {Etiqueta = "Sencillos de un álbum", Tipo = TipoReporte.ListaCancionesAlbum},
+                };
+            return modelo;
         }
     }
 }
---
+++ ProyectoFinal/Politecnico.Patrones.ProyectoFinal.Web/Models/ReporteDefinicion.cs
@@ -0,0 +1,13 @@
+﻿using Politecnico.Patrones.ProyectoFinal.Contratos.VO;
+
+namespace Politecnico.Patrones.ProyectoFinal.Web.Models {
+
+    public class ReporteDefinicion {
+        public string Etiqueta { get; set; }
+        public TipoReporte Tipo { get; set; }
+        public bool PermitirPdf { get; set; }
+        public ReporteDefinicion() {
+            PermitirPdf = true;
+        }
+    }
+}
